62a40385dfe1254be2221a72a093500a4aacd5af
Aaron Joseph Mathew <aaronjosephmathew@gmail.com>
2019-11-08 16:06:40 +0530

    Adding Python Question

---
+++ Python_Set/Q_List_Questions.py
@@ -0,0 +1,13 @@
+# For the lists below, print the combination of two lists
+color_list1 = ['green', 'red', 'blue', 'yellow']
+color_list2 = ['dark', 'bright', 'tinted', 'glowing']
+
+""" 
+Similar to the output below
+['dark green', 'dark red', 'dark blue', 'dark yellow'],
+['bright green', 'bright red', 'bright blue', 'bright yellow'], 
+['tinted green', 'tinted red', 'tinted blue', 'tinted yellow'], 
+['glowing green', 'glowing red', 'glowing blue', 'glowing yellow']
+"""
+
+
---
+++ Python_Set/S_List_Questions.py
@@ -0,0 +1,20 @@
+# For the lists below, print the combination of two lists
+color_list1 = ['green', 'red', 'blue', 'yellow']
+color_list2 = ['dark', 'bright', 'tinted', 'glowing']
+
+""" 
+Similar to the output below
+['dark green', 'dark red', 'dark blue', 'dark yellow'],
+['bright green', 'bright red', 'bright blue', 'bright yellow'], 
+['tinted green', 'tinted red', 'tinted blue', 'tinted yellow'], 
+['glowing green', 'glowing red', 'glowing blue', 'glowing yellow']
+"""
+
+# Solutions
+[[color2 + ' ' + color1 for color1 in color_list1] for color2 in color_list2]
+
+# Solution
+for i in color_list1:
+    for j in color_list2:
+        print(i,j)
+
