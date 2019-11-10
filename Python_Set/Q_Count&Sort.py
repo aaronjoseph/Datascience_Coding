72f0b3c6cddd97199fe543a1b10f6a3880ba270a
Aaron Joseph Mathew <aaronjosephmathew@gmail.com>
2019-11-10 16:29:39 +0530

    Adding Abhisheks Question

---
+++ Python_Set/Q_Count&Sort.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 10:29:06 2019
+
+@author: abhishekkaleidofin
+"""
+
+# Input format: A single line of input containing the string s
+# Output format: 1) Print the three most common characters along with their occurrence count each on a separate line.
+#                2) Sort output in descending order of occurrence count.
+#                3) If the occurrence count is the same, sort the characters in alphabetical order.
+
+# Sample Input: s = 'aabbbccde'
+
+# Sample Output: b 3
+#                a 2
+#                c 2
