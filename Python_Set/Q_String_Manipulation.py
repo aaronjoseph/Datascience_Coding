ccbdf35aefef52192225d241b185fa7346476bd1
Aaron Joseph Mathew <aaronjosephmathew@gmail.com>
2019-11-10 14:47:17 +0530

    Adding String Manipulation Code

--- Python_Set/Q_String_Manipulation.py
+++ Python_Set/Q_String_Manipulation.py
@@ -1,5 +1,14 @@
-s = "1#shyam#ceg#datascience#ml,2#robie#psg#physics#economy#daTASCIence#maths," \
-    "3#vignesh#ceg#robotics#maths#economy,4#preeth#ceg#geology#english#maths" \
+s = "1#shyam#datascience#ml,2#robie#physics#economy#daTASCIence#maths," \
+    "3#vignesh#robotics#maths#economy,4#preeth#geology#english#maths" \
     "#datascience#timeseries"
 
-Get the distribution of area_of_interest in this string
+# Get the distribution of area_of_interest in this string
+
+# Step 1 : Seperate the main section by (,)
+
+
+# Step 2 : Seperate the elements by ('#')
+
+
+# Step 3 : Provide the count of different elements
+
